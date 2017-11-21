e2fa84ce5b64be9757df6b279be35e5e02521873
Penguinang <1157703273@qq.com>
2017-11-21 12:50:55 +0800

    refactor($View): 改变了view之间的流程，完成了开始游戏部分view的跳转
    1. mainview 的 startbutton 添加了观察者的机制，需要监听 startbutton 点击事件的(如controller) 需实现 startbuttonobserver
    接口，并使用 mainview 的 addstartbuttonobserver 方法订阅点击事件
    
    改变了test.java

--- src/FlightChess/View/GameView.java
+++ src/FlightChess/View/GameView.java
@@ -5,13 +5,29 @@ import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class GameView extends JPanel{
     public GameView(){
         this.setMinimumSize(new Dimension(500,500));
-        JButton button = new JButton("开始游戏StartGame");
-        button.setFont(new Font("宋体",Font.PLAIN,24));
-        this.add(button);
+        JTextArea test = new JTextArea("开始游戏StartGame");
+        test.setFont(new Font("宋体",Font.PLAIN,24));
+        this.add(test);
+        this.setVisible(false);
+    }
+
+    /**
+     * Init game view
+     */
+    public void InitGameView(){
+        this.add(new JButton("init success"));
+    }
+
+    /**
+     * Show Game View.after gameView have  inited successfully
+     */
+    public void ShowGame(){
+        this.setVisible(true);
     }
 }
---
+++ src/FlightChess/View/testView.java
@@ -0,0 +1,29 @@
+package FlightChess.View;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class testView implements StartButtonObserver{
+    private MainView mainView;
+    public void main(String[] args){
+        System.out.println("Hello World");
+        
+        JFrame frame = new JFrame("Aeroplane Chess");
+        frame.setMinimumSize(new Dimension(500,500));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mainView = new MainView();
+        frame.setContentPane(mainView);
+        frame.setVisible(true);
+
+        mainView.AddStartObserver(this);
+    }
+
+    @Override
+    public void OnGameStarted(){
+        // MainController -> Game -> mainView
+        mainView.StartGame();
+    };
+}
