e7b90bb5f6271cf632b8520d66443d0339cc2a07
yichen yang <994986061@qq.com>
2017-12-04 17:27:25 +0800

    Model包

--- src/FlightChess/Model/Boat.java
+++ src/FlightChess/Model/Boat.java
@@ -1,20 +1,34 @@
 package FlightChess.Model;
 
-import FlightChess.View.GameView;
+/**
+ * 小船类
+ */
 
 public class Boat {
-	
+	/** 每个小船对应的ID */
 	private int id; 
+	/** 小船当前的位置 */
 	private int current_pos;
-	private int isBost;
+	/** 是否被禁足一轮 */
+	private boolean grounded;
 	
-	public Boat(int id, int current_pos) {
-		this.id = id;
-		this.current_pos = current_pos;
+	/**
+	 * 小船构造函数
+	 * @param i 小船ID
+	 * @param cp 小船初始位置
+	 */
+	public Boat(int i, int cp) {
+		this.id = i;
+		this.current_pos = cp;
+		this.grounded = false;
 	}
 	
+	/**
+	 * 使船在海中前进,更新船的当前位置
+	 * @param i 船在海中前进的步数
+	 */
 	public void advance(int i) {
-		this.current_pos +=i;
+		this.current_pos=(this.current_pos+i)%35;
 	}
 	
 	
@@ -62,7 +76,16 @@ public class Boat {
 	public void setCurrent_pos(int current_pos) {
 		this.current_pos = current_pos;
 	}
-	
+
+	public boolean isGrounded() {
+		return grounded;
+	}
+
+	public void setGrounded(boolean grounded) {
+		this.grounded = grounded;
+	}
+
+
 	
 	
 
